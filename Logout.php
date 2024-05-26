1ade4b3f5c80ad70d60923b3d3de5be05aa37589
Amirul Aidil <90623526+Mirulaidl@users.noreply.github.com>
2024-05-26 14:39:51 +0800

    update logout

--- Logout.php
+++ Logout.php
@@ -1,16 +1,6 @@
 <?php
     session_start();
-    if(basename($_SERVER['PHP_SELF']) == "adminDashboard.php"){
-        unset($_SESSION["Admin"]);
-        unset($_SESSION["AdminName"]);
-    }else if(basename($_SERVER['PHP_SELF']) == "userDashboard.php"){
-        unset($_SESSION["User"]);
-        unset($_SESSION["UserType"]);
-        unset($_SESSION["UserName"]);
-    }else if(basename($_SERVER['PHP_SELF']) == "ukDashboard.php"){
-        unset($_SESSION["UnitKeselamatan"]);
-        unset($_SESSION["UKName"]);
-    }  
+    session_unset();
     session_destroy();
     header("Location:login.php");
 ?>
