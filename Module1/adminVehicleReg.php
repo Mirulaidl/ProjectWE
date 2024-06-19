9311e8c20651a7ba099fd5c8cc9e197aa3994b88
Amirul Aidil <90623526+Mirulaidl@users.noreply.github.com>
2024-06-19 11:38:18 +0800

    admin

---
+++ Module1/adminVehicleReg.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Vehicle Registration</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../Module2/styleAdmin.css">
+    <!-- Bootstrap -->
+    <?php 
+        session_start();
+        include '../includes/connect.php';
+        include '../includes/bootstrap.php';
+    ?>
+</head>
+<?php include '../includes/headerLoggedIn.php'; ?>
+<body>
+    
+</body>
+
+</html>
